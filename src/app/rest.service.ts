8abd0c43bd956722f3ecd8bf5da6a0f1adf57e62
mckerney <jim.mckerney@intel.com>
2021-10-22 00:12:51 -0700

    routing change

--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -1,16 +1,20 @@
 import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { Router } from "@angular/router";
 
 @Injectable({
     providedIn: 'root'
 })
 export class RestService {
 
-    constructor(private http : HttpClient) { }
+    constructor(
+        private http : HttpClient,
+        private router: Router
+    ) { }
 
     ngOnInit() { }
 
-    predictionUrl : string = 'http://localhost:5000/prediction/';
+    predictionUrl : string = this.router.url + '/prediction/';
 
     getPrediction() {
         return this.http.get(this.predictionUrl);
