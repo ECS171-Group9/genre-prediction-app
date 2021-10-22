12f768b3ab3b564952fffa1b3b99fa129b4096e6
Jim McKerney <jim.mckerney@intel.com>
2021-10-22 00:50:09 -0700

    Update flask arguments

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-app = Flask(__name__, static_url_path='', static_folder='dist', template_folder='dist')
+app = Flask(__name__)
 CORS(app)
 
 
