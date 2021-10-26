e18ff4687f7f024e29e4bdcdbb5bb12dccfe8a39
unknown <jeremyshawnjackson@gmail.com>
2021-10-26 12:47:37 -0700

    Added bootstrap, stickied footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule} from "@angular/common/http";
 import { RestService } from "./rest.service";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     declarations: [
@@ -13,7 +14,8 @@ import { RestService } from "./rest.service";
     imports: [
         BrowserModule,
         AppRoutingModule,
-        HttpClientModule
+        HttpClientModule,
+        NgbModule
     ],
     providers: [RestService],
     bootstrap: [AppComponent]
