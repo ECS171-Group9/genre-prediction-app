8f337a4c9dade09f7985426cb2a7d95b4c63e0c0
mckerney <jim.mckerney@intel.com>
2021-11-12 18:13:51 -0800

    requirements update flatbuffers and removed aws keys

--- backend/configs.py
+++ backend/configs.py
@@ -2,8 +2,8 @@
 summary_size = 250
 
 # credentials for aws s3 access
-aws_access_key = "AKIAX7KKY5GZWDZ5R3XS"
-aws_secret_key = "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"
+aws_access_key = "temp"
+aws_secret_key = "temp"
 
 # URLs for S3 assets
 model_url = "https://genre-prediction-assets.s3.us-west-1.amazonaws.com/book_model.h5"
