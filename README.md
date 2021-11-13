845a690e9176578e1b1bce584342bcb632f96259
mckerney <jim.mckerney@intel.com>
2021-11-12 17:45:49 -0800

    rolling back pre model file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Book Genre Prediction App
 
-This application utilizes a machine learning model developed by Group 9 in ECS 171 Fall 2021 to take a book description as input and return a predicted genre of book.
+This application utilizes a machine learning model developed by Group 9 in ECS 171 Fall 2021 to take a book description/summary as input and return the predicted genre(s) of the book.
 
 https://book-genre-prediction.herokuapp.com/
 
@@ -12,7 +12,16 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 ## Development server
 
-Run `gunicorn wsgi:app` with appropriate dependencies installed to run application locally, this is also how it is deployed on Heroku. Make sure to use `ng build --configuration=production` prior to launching the webserver (gunicorn) for the first time. You also want to re-build after you make any changes locally for them to show up when you restart the webserver.
+Instructions for Linux:
+In the terminal go to the project directory, I recommend setting up a virtual environment to keep the package dependencies controlled, you will need to have `nodejs` and `npm` installed in order to handle the packages. At that point, you should be able to run `npm install` to download package dependecies that are outlined in `packages.json`.
+
+Once packages are installed, run `ng build --configuration=production` in order to build the application. After that, run `gunicorn wsgi:app` to host the application locally. Depending on how you port bind `gunicorn` it should be available at `localhost:8000` in your web-browser. 
+
+You also want to re-build and restart the webserver, in that order, if you want to see any changes you've made to the application appear in the web-browser.
+
+Not sure if this will work the same for MacOS, in that I am not sure if `npm` will work the same way to download dependecies, once you have the appropriate packages it should be the same though. If you are on Windows I recommend using WSL/WSL2 to run this, which is what I use for all this anyhow.
+
+For application debug `gunicorn wsgi:app --capture-output --log-level debug --timeout 90` to run the app will output `gunicorn` logs as well as the debug logs in the app itself.
 
 ## Code scaffolding
 
@@ -21,15 +30,5 @@ Run `ng generate component component-name` to generate a new component. You can 
 ## Build
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,48 @@
+"""
+Created for Group 9's group project in ECS 171 Fall 2021
+"""
+import re
+import pickle
+import logging
+from .configs import *
+import tempfile
+
+import boto3
+import numpy as np
 from flask import Flask, jsonify, send_from_directory, request
+from tensorflow.keras.models import load_model
 
 
 app = Flask(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def download_model(aws_filename=model_filename, access_key=aws_access_key,
+                   secret_key=aws_secret_key, bucket_name=aws_bucket) -> any:
+    """
+    Retrieve Neural Network model from AWS S3 storage. Store in a tempfile for download that will be destroyed
+    once it is loaded into memory
+    :param aws_filename: file name of the model
+    :param access_key: AWS credential key
+    :param secret_key: AWS credential secret key
+    :param bucket_name: name of bucket that the model is stored in on AWS S3
+    :return: returns Keras neural network model ready for prediction
+    """
+    with tempfile.NamedTemporaryFile() as temp:
+        s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key,
+                          region_name='us-west-1')
+        s3.download_file(bucket_name, aws_filename, temp.name)
+        logging.debug("Download Successful!")
+        return load_model(temp.name)
+
+
+# load needed assets into memory
+model = download_model()
+vocabulary = pickle.load(open('backend/vocab_dict.pkl', 'rb'))
+mlb = pickle.load(open('backend/mlb.pkl', 'rb'))
+
+summary_length = summary_size
 
 
 @app.route('/<path:path>', methods=['GET'])
@@ -18,6 +59,7 @@ def root():
 def get_prediction():
     request_data = request.get_json()
     summary = request_data.get('data')
+    logging.debug(f'summary: {summary}')
     try:
         genre = prediction(summary)
         return jsonify({'data': genre})
@@ -26,12 +68,97 @@ def get_prediction():
         app.logger.error(f'Exception occurred retrieving summary. {e}')
 
 
-def prediction(summary: str) -> str:
-    # TODO: Will need to add model implementation here
-    # TODO: Need to add a function to clean the summary to shape it like the data the model is trained on
-    # testing summary value
-    genre = summary
+def prediction(summary: str) -> list:
+    """
+    Run cleaning processes and return a list of the predicted genres for the summary
+    :param summary: text description of a book
+    :return: list of possible predicted genres
+    """
+
+    tokenized_summary = summary_preprocessing(summary)
+
+    logging.debug(f'tokenized_summary: {tokenized_summary}')
+    logging.debug(f'Type: {type(tokenized_summary)}, Shape: {np.asarray(tokenized_summary).shape} ')
+
+    prediction_summary = np.reshape(tokenized_summary, (1, summary_length))
+    logging.debug(f'prediction_summary: {prediction_summary}')
+
+    output = model.predict(prediction_summary).tolist()
+    logging.debug(f'output: {output}')
+
+    score = [[(value > 0.2)*1 for value in element] for element in output]
+    logging.debug(f'score: {score}')
+
+    genre = mlb.inverse_transform(np.asarray(score))
+    logging.debug(f'genre: {genre}')
     return genre
+
+
+def summary_preprocessing(summary: str) -> list:
+    """
+    Function takes a submitted summary and performs the preprocessing necessary for our prediction model
+
+    :param summary: str of words summarizing a book
+    :return:
+    """
+    clean_summary = clean_text(summary)
+
+    logging.debug(f'clean_summary: {clean_summary}')
+
+    tokenized_summary = tokenizer(summary=clean_summary, vocab_dict=vocabulary, summary_length=summary_length)
+    return tokenized_summary
+
+
+def tokenizer(summary, vocab_dict, summary_length) -> list:
+    """
+    Function that tokenizes a book summary to use on a genre prediction model
+
+    :param summary: Summary to convert to tokens
+    :param vocab_dict: Vocabulary used to train the model, dictionary mapping words to tokens
+    :param summary_length: Length of the summary
+    :return:  List of length summary_length, pre-padded with zeroes if the desc length was less than max_desc_length
+    """
+
+    token_list = [vocab_dict[i] if i in vocab_dict else 0 for i in summary.split()]
+
+    logging.debug(f'token_list: {token_list}')
+    padding = [0] * summary_length
+    if len(token_list) < summary_length:
+        return np.asarray(padding[:summary_length - len(token_list)] + token_list).squeeze()
+    else:
+        return np.asarray(token_list[:summary_length]).squeeze()
+
+
+def clean_text(text: str) -> str:
+    """
+    Clean contractions from the text
+    :param text: summary to clean up
+    :return: summary with contractions converted
+    """
+    text = text.lower()
+    text = re.sub(r"what's", "what is ", text)
+    text = text.replace('(ap)', '')
+    text = re.sub(r"\'s", " is ", text)
+    text = re.sub(r"\'ve", " have ", text)
+    text = re.sub(r"can't", "cannot ", text)
+    text = re.sub(r"n't", " not ", text)
+    text = re.sub(r"i'm", "i am ", text)
+    text = re.sub(r"\'re", " are ", text)
+    text = re.sub(r"\'d", " would ", text)
+    text = re.sub(r"\'ll", " will ", text)
+    text = re.sub(r'\W+', ' ', text)
+    text = re.sub(r'\s+', ' ', text)
+    text = re.sub(r"\\", "", text)
+    text = re.sub(r"\'", "", text)
+    text = re.sub(r"\"", "", text)
+    text = re.sub('[^a-zA-Z ?!]+', '', text)
+    text = _remove_non_ascii(text)
+    text = text.strip()
+    return text
+
+
+def _remove_non_ascii(summary: str) -> str:
+    return "".join(i for i in summary if ord(i) < 128)
 
 
 if __name__ == '__main__':
